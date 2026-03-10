233d0f05a7dfd0ddabae98a84ae15ad5962821b5
Shritej-Anil-Chavan <chavanshri568@gmail.com>
2026-03-10 11:24:23 +0530

    Springboot project setup

---
+++ src/test/java/com/securepasswordmanager/securepasswordmanager/SecurePasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securepasswordmanager.securepasswordmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePasswordManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
